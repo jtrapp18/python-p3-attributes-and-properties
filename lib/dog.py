32fd9eeae694f0ec0d0a18871587bfe05584a55c
+ <jacqueline.trapp18@gmail.com>
2024-12-11 13:45:15 -0500

    lab complete

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,22 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    
+    def __init__(self, name=None, breed=None):
+        self.name = name
+        self.breed = breed
+
+        if name is None:
+            pass
+        elif isinstance(name, str) and (len(name) > 0) and (len(name) < 26):
+            print(f"Setting name to {name}.")
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+        if breed is None:
+            pass
+        elif breed in APPROVED_BREEDS:
+            self.breed = breed            
+        else:
+            print("Breed must be in list of approved breeds.")
+
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,21 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+
+    def __init__(self, name=None, job=None):
+        self.name = name
+        self.job = job
+
+        if name is None:
+            pass
+        elif isinstance(name, str) and (len(name) > 0) and (len(name) < 26):
+            self.name = " ".join([n.capitalize() for n in name.split(" ")])
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+        if job is None:
+            pass
+        elif job in APPROVED_JOBS:
+            self.job = job            
+        else:
+            print("Job must be in list of approved jobs.")
